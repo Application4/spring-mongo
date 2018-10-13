63cffeee459124a31d8743091155d0eca8678554
basahota <techbasanta757@gmail.com>
2018-10-14 00:59:02 +0530

    removed multi parm

--- src/main/java/com/spring/mongo/example/SpringMongoApplication.java
+++ src/main/java/com/spring/mongo/example/SpringMongoApplication.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.spring.mongo.example.dao.EmployeeRepository;
 import com.spring.mongo.example.model.Employee;
@@ -19,10 +17,8 @@ public class SpringMongoApplication {
 	@Autowired
 	private EmployeeRepository repository;
 
-	@GetMapping("/save/{id}/{name}/{dept}/{salary}")
-	public String save(@PathVariable int id, @PathVariable String name, @PathVariable String dept,
-			@PathVariable double salary) {
-		Employee employee = new Employee(id, name, dept, salary);
+	@GetMapping("/save")
+	public String save(@RequestBody Employee employee) {
 		repository.save(employee);
 		return "Employee saved successfully...";
 	}
